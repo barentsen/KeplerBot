ec102750774c9ba5dbc773b9ea4743998def8aae
Geert Barentsen <geert@barentsen.be>
2018-09-13 21:48:57 -0700

    Add a quicklook lightcurve feature!

--- keplerbot.py
+++ keplerbot.py
@@ -16,10 +16,13 @@ from astropy.coordinates import SkyCoord
 from k2flix import TargetPixelFile
 from k2flix.crawler import KeplerArchiveCrawlerDB
 
+import matplotlib.pyplot as pl
+from lightkurve import KeplerTargetPixelFile
+
 from secrets import *
 
 
-def generate_tweet(tpf_fn=None, movie_length=80):
+def generate_tweet(tpf_fn=None, movie_length=60, step=49):
     """Generate a status message and animated gif.
 
     Parameters
@@ -32,7 +35,7 @@ def generate_tweet(tpf_fn=None, movie_length=80):
 
     Returns
     -------
-    (status, gif, tpf) : (str, str, `TargetPixelFile`)
+    (status, gif, plot, tpf) : (str, str, str, `TargetPixelFile`)
     """
     # Open the Target Pixel File
     if tpf_fn is None:  # Get a random url
@@ -51,7 +54,7 @@ def generate_tweet(tpf_fn=None, movie_length=80):
     while attempt_no < 7:
         attempt_no += 1
         try:
-            start = random.randint(0, tpf.no_frames - movie_length)
+            start = random.randint(0, tpf.no_frames - step*movie_length)
             try:
                 kepmag = '🔆 Kp {:.1f}\n'.format(float(tpf.hdulist[0].header['KEPMAG']))
             except Exception:
@@ -59,7 +62,7 @@ def generate_tweet(tpf_fn=None, movie_length=80):
             timestr = tpf.timestamp(start).split(' ')[0]
             campaign = tpf.hdulist[0].header['CAMPAIGN']
             url = "https://archive.stsci.edu/k2/data_search/search.php?ktc_k2_id={}&action=Search".format(tpf.objectname.split(' ')[1])
-            status = ('New Kepler/K2 data were recently released!\n'
+            status = ('New Kepler data were recently released!\n'
                       '🔎 {}\n'
                       '🗓 {} (C{})\n'
                       '{}'
@@ -67,34 +70,47 @@ def generate_tweet(tpf_fn=None, movie_length=80):
             log.info(status)
             # Create the animated gif
             gif_fn = '/tmp/keplerbot.gif'
-            tpf.save_movie(gif_fn, start=start, stop=start + movie_length,
-                           step=1, fps=8, min_percent=0., max_percent=93.,
+            tpf.save_movie(gif_fn, start=start, stop=start + step*movie_length,
+                           step=step, fps=3, min_percent=0., max_percent=93.,
                            ignore_bad_frames=True)
-            return status, gif_fn, tpf
+            # Create the lightcurve plot
+            plot_fn = '/tmp/keplerbot-lightcurve.png'
+            ktpf = KeplerTargetPixelFile(tpf_fn)
+            ktpf.to_lightcurve(aperture_mask='all').correct(restore_trend=True).remove_outliers().scatter(normalize=False)
+            pl.tight_layout()
+            print('Writing {}'.format(plot_fn))
+            pl.savefig(plot_fn)
+            pl.close()
+
+            return status, gif_fn, plot_fn, tpf
         except Exception as e:
-            log.warning(e)
+            log.error(e)
     raise Exception('Tweet failed')
 
 
-def post_tweet(status, gif):
+def post_tweet(status, gif, plot):
     """Post an animated gif and associated status message to Twitter."""
     twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
     upload_response = twitter.upload_media(media=open(gif, 'rb'))
+    upload_response2 = twitter.upload_media(media=open(plot, 'rb'))
     response = twitter.update_status(status=status, media_ids=upload_response['media_id'])
+    status2 = '...and here is the quicklook lightcurve:'
+    response2 = twitter.update_status(status=status2, media_ids=upload_response2['media_id'],
+                                      in_reply_to_status_id=response['id'])
     log.info(response)
-    return twitter, response
+    return twitter, response, response2
 
 
 if __name__ == '__main__':
-    attempt_no = 9
+    attempt_no = 0
     while attempt_no < 10:
         attempt_no += 1
         try:
-            status, gif, tpf = generate_tweet()
+            status, gif, plot, tpf = generate_tweet()
             if 'test' in sys.argv:
                 print('Running in test mode -- not posting to Twitter.')
             else:
-                twitter, response = post_tweet(status, gif)
+                twitter, response, response2 = post_tweet(status, gif, plot)
             break
         except Exception as e:
             log.warning(e)
